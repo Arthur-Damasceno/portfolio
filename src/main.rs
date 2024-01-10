447c917f1adf1e6d730d155bd2a9d831bfc6252c
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-01-10 10:53:12 -0300

    feat: yew setuped

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use yew::{prelude::*, Renderer};
+
+#[function_component]
+fn App() -> Html {
+    let hello_world = "Hello World!";
+
+    html! {
+        <div>
+            <p>{ hello_world }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Renderer::<App>::new().render();
 }
